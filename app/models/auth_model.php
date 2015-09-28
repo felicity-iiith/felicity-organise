10cef9c3d336b9401b243060dc148102d145cc35
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2015-09-29 02:14:48 +0530

    Make $admins in config optional

--- app/models/auth_model.php
+++ app/models/auth_model.php
@@ -5,6 +5,10 @@ class auth_model extends Model {
     function is_admin($user) {
         global $admins;
 
+        if (!isset($admins) || !is_array($admins)) {
+            return false;
+        }
+
         if (in_array($user, $admins)) {
             return true;
         }
