da582e068c013652cad7b62296873b4c10c0802d
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2015-11-29 22:53:47 +0530

    Update DB structure

--- db/database.sql
+++ db/database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.0-dev
+-- version 4.5.2-dev
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 21, 2015 at 02:17 AM
--- Server version: 5.5.44-0ubuntu0.14.04.1-log
--- PHP Version: 5.5.9-1ubuntu4.13
+-- Generation Time: Nov 29, 2015 at 10:50 PM
+-- Server version: 5.5.46-0ubuntu0.14.04.2-log
+-- PHP Version: 5.5.9-1ubuntu4.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -22,11 +22,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `files` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `slug` varchar(255) NOT NULL,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `slug` varchar(255) COLLATE utf8_bin NOT NULL,
   `parent` int(11) NOT NULL,
-  `type` enum('directory','file') NOT NULL,
-  `default_role` enum('none','reader','author','admin') NOT NULL DEFAULT 'reader',
+  `type` enum('directory','file') COLLATE utf8_bin NOT NULL,
+  `default_role` enum('none','reader','author','admin') COLLATE utf8_bin NOT NULL DEFAULT 'reader',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uniqueName` (`slug`,`parent`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
@@ -35,8 +35,9 @@ CREATE TABLE IF NOT EXISTS `files` (
 -- Dumping data for table `files`
 --
 
-INSERT INTO `files` (`id`, `name`, `slug`, `parent`, `type`) VALUES
-(0, 'Felicity ''16', '', -1, 'directory');
+INSERT INTO `files` (`id`, `name`, `slug`, `parent`, `type`, `default_role`) VALUES
+(0, 'Felicity ʼ16', '', -1, 'directory', 'reader');
+
 -- --------------------------------------------------------
 
 --
@@ -46,11 +47,11 @@ INSERT INTO `files` (`id`, `name`, `slug`, `parent`, `type`) VALUES
 CREATE TABLE IF NOT EXISTS `file_data` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `file_id` int(11) NOT NULL,
-  `action` enum('create','edit','delete','recover') NOT NULL,
-  `data` blob NOT NULL,
+  `action` enum('create','edit','delete','recover') COLLATE utf8_bin NOT NULL,
+  `data` text COLLATE utf8_bin NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `created_by` varchar(255) NOT NULL,
-  `comment` varchar(255) DEFAULT NULL,
+  `created_by` varchar(255) COLLATE utf8_bin NOT NULL,
+  `comment` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -63,8 +64,8 @@ CREATE TABLE IF NOT EXISTS `file_data` (
 CREATE TABLE IF NOT EXISTS `file_permissions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `file_id` int(11) NOT NULL,
-  `user` varchar(255) NOT NULL,
-  `role` enum('none','reader','author','admin') NOT NULL,
+  `user` varchar(255) COLLATE utf8_bin NOT NULL,
+  `role` enum('none','reader','author','admin') COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `uniqueUser` (`file_id`,`user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
@@ -78,12 +79,12 @@ CREATE TABLE IF NOT EXISTS `file_permissions` (
 CREATE TABLE IF NOT EXISTS `trash_files` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `file_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `slug` varchar(255) NOT NULL,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `slug` varchar(255) COLLATE utf8_bin NOT NULL,
   `parent` int(11) NOT NULL,
-  `type` enum('directory','file') NOT NULL,
+  `type` enum('directory','file') COLLATE utf8_bin NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `created_by` varchar(255) NOT NULL,
+  `created_by` varchar(255) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `file_id` (`file_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
